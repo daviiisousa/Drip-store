b59810a1e906abbb0ae7db8be8963677a860983c
daviiisousa <sousadavi248@gmail.com>
2024-10-24 14:42:37 -0300

    comecando section carrosel

--- src/Components/SectionCarrosel/SectionCarrosel.jsx
+++ src/Components/SectionCarrosel/SectionCarrosel.jsx
@@ -1,8 +1,24 @@
 import './SectionCarrosel.css'
+import SapatoCarrosel from '../../assets/HomeFotos/Sapatocarrosel.png'
 
 export const SectionCarrosel = () => {
     return ( 
         <>
+        <section className='sectionCarrosel'>
+            <div>
+                <p>Melhores ofertas personalizadas</p>
+                <h2>
+                Queima de stoque Nike 🔥
+                </h2>
+                <p>
+                Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.
+                </p>
+                <button>teste</button>
+            </div>
+            <div>
+                <img src={SapatoCarrosel} alt="" />
+            </div>
+        </section>
         </>
      );
 }
