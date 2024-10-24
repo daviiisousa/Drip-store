8219a7a7c27f6f69bca709af41271a7e33416824
Thiudiaz <thiudiaz02@gmail.com>
2024-10-24 14:07:45 -0300

    add foto

--- src/Components/SectionOfertaEspecial/SectionOfertaEspecial.jsx
+++ src/Components/SectionOfertaEspecial/SectionOfertaEspecial.jsx
@@ -1,8 +1,18 @@
 import './SectionOfertaEspecial.css'
+import SapatoEspecial from '../../assets/HomeFotos/SapatoEspecial.png'
 
 export const SectionOfertaEspecial = () => {
     return ( 
-        <>
+        <> 
+        <section>
+            <div>
+                <img src={SapatoEspecial} alt="" />
+
+            </div>
+            <div>
+
+            </div>
+        </section>
         </>
      );
 }
