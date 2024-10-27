21795bfe46e336f785c51f3fe99e926064e9a98c
Rhuann <rhuannsilv@gmail.com>
2024-10-27 20:15:05 -0300

    Criação de cards

--- src/Components/SectionColecaoDestaque/SectionColecaoDestaque.jsx
+++ src/Components/SectionColecaoDestaque/SectionColecaoDestaque.jsx
@@ -1,8 +1,34 @@
-import './SectionColecaoDestaque.css'
+import CardColecaoDestaque from "../CardColecaoDestaque/CardColecaoDestaque";
+import "./SectionColecaoDestaque.css";
 
 export const SectionColecaoDestaque = () => {
-    return ( 
-        <>
-        </>
-     );
-}
+  return (
+    <div className="Colecao">
+      <h2>Coleções em destaque</h2>
+
+      {/* Cards de Coleção */}
+      <div className="Col">
+        <CardColecaoDestaque
+          label="Novo drop Supreme"
+          img="link-para-imagem-supreme"
+          discount="30% OFF"
+        />
+
+        <CardColecaoDestaque
+          label="Coleção Adidas"
+          img="link-para-imagem-adidas"
+          discount="30% OFF"
+        />
+
+        <CardColecaoDestaque
+          label="Novo Beats Bass"
+          img="link-para-imagem-beats"
+          discount="30% OFF"
+        />
+      </div>
+
+      {/* Ícones de Categoria */}
+      <h2>Coleções em destaque</h2>
+    </div>
+  );
+};
