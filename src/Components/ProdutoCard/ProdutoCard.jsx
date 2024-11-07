77c246e44861c67fa6dc73c787131fa267a0aefc
daviiisousa <sousadavi248@gmail.com>
2024-11-06 23:07:19 -0300

    branch teste com api de produto rodando

--- src/Components/ProdutoCard/ProdutoCard.jsx
+++ src/Components/ProdutoCard/ProdutoCard.jsx
@@ -1,102 +1,55 @@
-import './ProdutoCard.css'
-import sapatoCard from '../../assets/HomeFotos/SapatoCard.png'
+import "./ProdutoCard.css";
+import { useEffect, useState } from "react";
 
 export const ProdutoCard = () => {
+  const [produtos, setProdutos] = useState([]);
 
-    const produtos = [
-        {
-            id: 1,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            desconto: "30% OFF",
-            imagem: sapatoCard
-          },
-          {
-            id: 2,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            desconto: "30% OFF",
-            imagem: sapatoCard
-          },
-          {
-            id: 3,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            imagem: sapatoCard
-          },
-          {
-            id: 4,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            imagem: sapatoCard
-          },
-          {
-            id: 5,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            imagem: sapatoCard
-          },
-          {
-            id: 6,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            imagem: sapatoCard
-          },
-          {
-            id: 7,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            imagem: sapatoCard
-          },
-          {
-            id: 8,
-            nome: "K-Swiss V8 - Masculino",
-            categoria: "Tênis",
-            precoAntigo: 200,
-            precoNovo: 100,
-            imagem: sapatoCard
-          }
-        ];
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/users/");
+        const data = await response.json();
+        console.log(data);
+        setProdutos(data);
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      }
+    };
 
-        const ProdutoCard = ({ nome, categoria, precoAntigo, precoNovo, desconto, imagem }) => (
-          <div className="produto">
-            {desconto && <div className="desconto">{desconto}</div>}
-            <img src={imagem} alt={nome} className="produto-imagem" />
-            <p className="categoria">{categoria}</p>
-            <p className="nome">{nome}</p>
-            <p className="preco-antigo">${precoAntigo}</p>
-            <p className="preco-novo">${precoNovo}</p>
-          </div>
-        );
-    return ( 
-        <>
-            <div className="produtos">
-              {produtos.map((produto) => (
-                <ProdutoCard
-                  key={produto.id}
-                  nome={produto.nome}
-                  categoria={produto.categoria}
-                  precoAntigo={produto.precoAntigo}
-                  precoNovo={produto.precoNovo}
-                  desconto={produto.desconto}
-                  imagem={produto.imagem}
-                />
-              ))} 
-            </div>
-        </>
-     );
-}
+    fetchProdutos();
+  }, []);
+
+  const ProdutoItem = ({
+    nome,
+    categoria,
+    precoAntigo,
+    precoNovo,
+    desconto,
+    imagem,
+  }) => (
+    <div className="produto">
+      {desconto && <div className="desconto">{desconto}% OFF</div>}
+      <img src={imagem} alt={nome} className="produto-imagem" />
+      <p className="categoria">{categoria}</p>
+      <p className="nome">{nome}</p>
+      <p className="preco-antigo">${precoAntigo}</p>
+      <p className="preco-novo">${precoNovo}</p>
+    </div>
+  );
+
+  return (
+    <div className="produtos">
+      {produtos.map((produto) => (
+        <ProdutoItem
+          key={produto.id}
+          nome={produto.nome}
+          categoria={produto.categoria}
+          precoAntigo={produto.precoAntigo}
+          precoNovo={produto.precoNovo}
+          desconto={produto.desconto}
+          imagem={produto.imagem}
+        />
+      ))}
+    </div>
+  );
+};
