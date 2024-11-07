817973814a46f1c05e7e8aa7512ecd93246cf64c
daviiisousa <sousadavi248@gmail.com>
2024-11-07 20:18:49 -0300

    produtos com a estilizada

--- src/Components/SectionProdutos/SectionProdutos.jsx
+++ src/Components/SectionProdutos/SectionProdutos.jsx
@@ -5,11 +5,18 @@ export const SectionProdutos = () => {
     return ( 
         <>
             <section className="sectionProduto">
+                <div className="divProdutoTextos">
+                    <h2>Resultados para “Tênis” -<span className="spanProdutos"> 389 produtos</span></h2>
+                    <select>
+                        <option>Ordernar por: mais relevantes</option>
+                    </select>
+                </div>
                 <div className="divProduto">
                     <aside className="filtro">
                         <h1>filtro</h1>
                     </aside>
                     <div className="Produtos">
+                        <ProdutoCard />
                         <ProdutoCard />
                     </div>
                 </div>
