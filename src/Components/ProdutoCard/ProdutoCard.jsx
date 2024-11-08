f751a05ec9aa0439d9ae149e218093c8f4ef8f2e
daviiisousa <sousadavi248@gmail.com>
2024-11-08 14:34:02 -0300

    crei um context para a api de produtos

--- src/Components/ProdutoCard/ProdutoCard.jsx
+++ src/Components/ProdutoCard/ProdutoCard.jsx
@@ -1,25 +1,12 @@
+import { ProductContext } from "../../context/ProductContext";
 import "./ProdutoCard.css";
-import { useEffect, useState } from "react";
-import "./ProdutoCard.css";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 
 export const ProdutoCard = () => {
-  const [produtos, setProdutos] = useState([]);
 
-  useEffect(() => {
-    const fetchProdutos = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/users/");
-        const data = await response.json();
-        console.log(data);
-        setProdutos(data);
-      } catch (error) {
-        console.error("Erro ao buscar produtos:", error);
-      }
-    };
+  const {produtos} = useContext(ProductContext)
 
-    fetchProdutos();
-  }, []);
+  const produtosLimitados = produtos.slice(0, 8)
 
   const ProdutoItem = ({
     nome,
@@ -29,7 +16,7 @@ export const ProdutoCard = () => {
     desconto,
     imagem,
   }) => (
-    <div className="produto">
+    <div className="Produto">
       {desconto && <div className="desconto">{desconto}% OFF</div>}
       <img src={imagem} alt={nome} className="produto-imagem" />
       <p className="categoria">{categoria}</p>
@@ -41,62 +28,7 @@ export const ProdutoCard = () => {
 
   return (
     <div className="produtos">
-      {produtos.map((produto) => (
-        <ProdutoItem
-          key={produto.id}
-          nome={produto.nome}
-          categoria={produto.categoria}
-          precoAntigo={produto.precoAntigo}
-          precoNovo={produto.precoNovo}
-          desconto={produto.desconto}
-          imagem={produto.imagem}
-        />
-      ))}
-    </div>
-  );
-};
-
-  const [produtos, setProdutos] = useState([]);
-
-  useEffect(() => {
-    const fetchProdutos = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/users/");
-        const data = await response.json();
-        const produtoLimitado = data.splice(0, 8)
-        console.log(data);
-        setProdutos(produtoLimitado);
-      } catch (error) {
-        console.error("Erro ao buscar produtos:", error);
-      }
-    };
-
-    fetchProdutos();
-  }, []);
-
-  const ProdutoItem = ({
-    nome,
-    categoria,
-    precoAntigo,
-    precoNovo,
-    desconto,
-    imagem,
-  }) => (
-    <div className="produto">
-      {desconto && <div className="desconto">{desconto}% OFF</div>}
-      <div className="divImagen">
-        <img src={imagem} alt={nome} className="produto-imagem" />
-      </div>
-      <p className="categoria">{categoria}</p>
-      <p className="nome">{nome}</p>
-      <p className="preco-antigo">${precoAntigo}</p>
-      <p className="preco-novo">${precoNovo}</p>
-    </div>
-  );
-
-  return (
-    <div className="produtos">
-      {produtos.map((produto) => (
+      {produtosLimitados.map((produto) => (
         <ProdutoItem
           key={produto.id}
           nome={produto.nome}
