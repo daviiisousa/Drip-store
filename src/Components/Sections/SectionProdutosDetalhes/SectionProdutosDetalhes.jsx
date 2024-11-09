667aac70535af3c055cd7bf9862f57608c6725b0
daviiisousa <sousadavi248@gmail.com>
2024-11-08 21:52:52 -0300

    puxando api n detailsproduct

--- src/Components/Sections/SectionProdutosDetalhes/SectionProdutosDetalhes.jsx
+++ src/Components/Sections/SectionProdutosDetalhes/SectionProdutosDetalhes.jsx
@@ -9,8 +9,17 @@ import sapatoBlack from "../../../assets/HomeFotos/black-sneaker.png";
 import sapatoBrow from "../../../assets/HomeFotos/brown-sneaker.png";
 import sapatoPurple from "../../../assets/HomeFotos/purple-sneaker.png";
 import { Navigation, Pagination } from "swiper/modules";
+import { BtnWarning } from "../../Buotoes/BtnWarning/BtnWarning";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ProductContext } from "../../../context/ProductContext";
+import { ProdutoItem } from "../../ProdutoItem/ProdutoItem";
 
 export const SectionProdutosDetalhes = () => {
+
+    const {produtos} = useContext(ProductContext)
+    const produtosLimitados = produtos.slice(0, 4)
+
   return (
     <>
       <section className="sectionDetails">
@@ -26,31 +35,54 @@ export const SectionProdutosDetalhes = () => {
                modules={[Pagination, Navigation]}
                className="mySwiper"
             >
-              <SwiperSlide>
+              <SwiperSlide className="red">
                 <DivFotosCarrosel style={{display: "none"}} className={"teste"}  fotoCarrosel={sapatoPadrao} />
               </SwiperSlide>
-              <SwiperSlide>
+              <SwiperSlide className="black">
                 <DivFotosCarrosel style={{display: "none"}} fotoCarrosel={sapatoBlack} />
               </SwiperSlide>
-              <SwiperSlide>
+              <SwiperSlide className="brow">
                 <DivFotosCarrosel style={{display: "none"}} fotoCarrosel={sapatoBrow} />
               </SwiperSlide>
-              <SwiperSlide>
+              <SwiperSlide className="purple">
                 <DivFotosCarrosel style={{display: "none"}} fotoCarrosel={sapatoPurple} />
               </SwiperSlide>
             </Swiper>
           </div>
-          <div className="textoDetalhes">
-               <h1>Tênis Nike Revolution 6 Next Nature Masculino</h1>
-               <p>Casual | Nike | REF:38416711</p>
-               <p>R$219,00</p>
-               <h2>Descrição do produto</h2>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+          <div className="divTextos">
+              <div className="textoDetalhes">
+                   <h1>Tênis Nike Revolution 6 Next Nature Masculino</h1>
+                   <p>Casual | Nike | REF:38416711</p>
+                   <p>R$219,00</p>
+                   <h2>Descrição do produto</h2>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+              </div>
+              <div className="tamanhos"></div>
+              <div className="cores"></div>
+              <div className="btn-container">
+                <BtnWarning texto={"Comprar"}/>
+              </div>
           </div>
-          <div className="tamanhos"></div>
-          <div className="cores"></div>
-          <div className="btn-container"></div>
       </section>
+      <div className="produtos-em-alta-header">
+        <h2>Produtos em alta</h2>
+        <Link to={"/produtos"} className="ver-todos">
+          Ver todos →
+        </Link>
+      </div>
+      <div className="produtos">
+        {produtosLimitados.map((produto) => (
+          <ProdutoItem
+            key={produto.id}
+            nome={produto.nome}
+            categoria={produto.categoria}
+            precoAntigo={produto.precoAntigo}
+            precoNovo={produto.precoNovo}
+            desconto={produto.desconto}
+            imagem={produto.imagem}
+          />
+        ))}
+      </div>
     </>
   );
 };
--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import { Footer } from "../../Components/Footer/Footer";
 import { Header } from "../../Components/Header/Header";
 import { SectionProdutosDetalhes } from "../../Components/Sections/SectionProdutosDetalhes/SectionProdutosDetalhes";
+import './ProductDetails.css'
 
 export const ProductDetails = () => {
     return ( 
@@ -8,7 +9,7 @@ export const ProductDetails = () => {
             <header>
                 <Header />
             </header>
-            <main>
+            <main className="mainProductDetails">
                 <SectionProdutosDetalhes />
             </main>
             <footer>
