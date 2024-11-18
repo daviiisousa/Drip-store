e9ca966e105f88b1775492575ef93e738712f0e5
daviiisousa <sousadavi248@gmail.com>
2024-11-18 11:29:54 -0300

    finalizar compra com estilo

--- src/Pages/FinalizarCompra/FinalizarCompra.jsx
+++ src/Pages/FinalizarCompra/FinalizarCompra.jsx
@@ -1,6 +1,8 @@
 import { Footer } from "../../Components/Footer/Footer";
 import { Header } from "../../Components/Header/Header";
 
+import './FinalizarCompra.css'
+
 export const FinalizarCompra = () => {
     return ( 
         <>
@@ -9,7 +11,31 @@ export const FinalizarCompra = () => {
                     <Header />
                 </header>
                 <main>
-                    <h1>Finalizar Compra</h1>
+                    <section className="sectionFinalizarComprar">
+                        <h1 className="tituloFinalizarComprar">Finalizar Compra</h1>
+                        <div className="divForm">
+                            <h2>Informações Pessoais</h2>
+                            <hr />
+                            <form action="">
+                                <div>
+                                    <label htmlFor="">Nome Completo *</label>
+                                    <input type="text" />
+                                </div>
+                                <div>
+                                    <label htmlFor="">CPF *</label>
+                                    <input type="text" />
+                                </div>
+                                <div>
+                                    <label htmlFor="">E-mail *</label>
+                                    <input type="text" />
+                                </div>
+                                <div>
+                                    <label htmlFor="">Celular *</label>
+                                    <input type="text" />
+                                </div>
+                            </form>
+                        </div>
+                    </section>
                 </main>
                 <footer>
                     <Footer />
